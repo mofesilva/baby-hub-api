e9ddb8664c876fa8765d07d46109a588a2e0a7aa
mofesilva <mofesilva.dev@gmail.com>
2025-11-26 17:28:06 -0300

    feat: add admin and babies routes to the main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import productsRouter from './products.js';
 import pledgesRouter from './pledges.js';
 import donationsRouter from './donations.js';
+import adminAuthRouter from './admin/auth.js';
+import adminUploadsRouter from './admin/uploads.js';
+import babiesRouter from './babies.js';
 
 const router = Router();
 
 router.use('/products', productsRouter);
 router.use('/pledges', pledgesRouter);
 router.use('/donations', donationsRouter);
+router.use('/admin/auth', adminAuthRouter);
+router.use('/babies', babiesRouter);
+router.use('/admin/uploads', adminUploadsRouter);
 
 export default router;
