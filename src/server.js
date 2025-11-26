3b336fd83ed9d78cb9775da584c079e69f7dac66
mofesilva <mofesilva.dev@gmail.com>
2025-11-26 17:26:42 -0300

    feat: add admin seeding during server startup

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ import app from './app.js';
 import config from './config/env.js';
 import { connectMongo } from './db/mongoose.js';
 import { seedProductsIfNeeded } from './repositories/products.js';
+import { seedAdminIfNeeded } from './repositories/users.js';
 
 const start = async () => {
   try {
     await connectMongo();
     await seedProductsIfNeeded();
+    await seedAdminIfNeeded();
 
     app.listen(config.port, () => {
       console.log(`API ouvindo na porta ${config.port}`);
